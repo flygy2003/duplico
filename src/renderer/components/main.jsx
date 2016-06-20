dbb1cd9b374475d914695aaf5bb9494a85f2ca21
Machisté Quintana <machiste@slack-corp.com>
2016-06-20 14:20:12 -0700

    Fix Main component import

--- src/renderer/components/main.jsx
+++ src/renderer/components/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shell} from 'electron';
 
-class Main extends React.Component {
+export default class Main extends React.Component {
   state = {
     message: 'Hello, Electron'
   }
@@ -16,11 +16,11 @@ class Main extends React.Component {
         <div className="jumbotron main">
           <h1>{this.state.message}</h1>
           <img src="../assets/images/electron.svg" alt="" width="128px"></img>
-          <p>Provided by <a href="#" onClick={this.openGithub}>electron-jsx-babel-boilerplate<span className="glyphicon glyphicon-heart"></span></a></p>
+          <p>
+            Provided by <a href="#" onClick={this.openGithub.bind(this)}>electron-jsx-babel-boilerplate<span className="glyphicon glyphicon-heart"></span></a>;
+          </p>
         </div>
       </div>
     );
   }
 }
-
-export default Main;
